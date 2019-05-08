2d70e6898a810add492eba4538ae86366109b4db
JanHoChoi <435504339@qq.com>
2019-05-08 19:16:36 +0800

    Update

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-[TOC]
-
 # LeetCode Solutions
 
 [![Travis](https://img.shields.io/badge/language-C++-red.svg)](https://blog.csdn.net/qq_39178023)
--- another.cpp
+++
@@ -1,9 +0,0 @@
-#include<vector>
-#include<iostream>
-using namespace std;
-
-int main(){
-    double temp = INT_MAX;
-        cout << temp << endl;
-    return 0;
-}
